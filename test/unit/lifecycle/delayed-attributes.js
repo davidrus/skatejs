a63d8f1b3cef4b11a9ea02b99530eb2ca8ce73a6
Ben Gummer <bengummer@gmail.com>
2016-10-11 16:34:38 +1100

    chore: added tests to demonstrate behaviour

--- test/unit/lifecycle/delayed-attributes.js
+++ test/unit/lifecycle/delayed-attributes.js
@@ -1,3 +1,4 @@
+import { define, prop, props, vdom } from '../../../src/index';
 import afterMutations from '../../lib/after-mutations';
 import element from '../../lib/element';
 import fixture from '../../lib/fixture';
@@ -61,6 +62,109 @@ describe('lifecycle/attributes', () => {
           done();
         });
       });
+    });
+  });
+
+  describe('property set by attribute provided in markup', () => {
+    function createElemMaybeWithLinkedAttrs(isLinked) {
+      const propObj = isLinked ? { attribute: true } : {};
+      const newElem = element();
+      const tagName = newElem.safe;
+
+      define(tagName, {
+        props: {
+          label: prop.string(propObj),
+          done: prop.boolean(propObj),
+        },
+      });
+
+      return tagName;
+    }
+
+    const testSets = [
+      {
+        summary: 'should not reflect attr to props if not linked',
+        outer: {
+          isLinked: false,
+          expectedDone: false,
+          expectedLabel: '',
+        },
+        inner: {
+          isLinked: false,
+          expectedDone: false,
+          expectedLabel: '',
+        },
+      },
+      {
+        summary: 'should reflect attr to props if linked',
+        outer: {
+          isLinked: true,
+          expectedDone: true,
+          expectedLabel: 'world',
+        },
+        inner: {
+          isLinked: false,
+          expectedDone: false,
+          expectedLabel: '',
+        },
+      },
+    ];
+
+    testSets.forEach((testSet) => {
+      it(testSet.summary, (done) => {
+        const outerTag = createElemMaybeWithLinkedAttrs(testSet.outer.isLinked);
+        const innerTag = createElemMaybeWithLinkedAttrs(testSet.inner.isLinked);
+
+        fixture(`
+          <${outerTag} label="world" done>
+            <${innerTag} label="world" done></${innerTag}>
+          </${outerTag}>
+        `);
+        afterMutations(() => {
+          const outerElem = document.querySelector(outerTag);
+          expect(outerElem.done).to.equal(testSet.outer.expectedDone);
+          expect(outerElem.label).to.equal(testSet.outer.expectedLabel);
+
+          const innerElem = outerElem.querySelector(innerTag);
+          expect(innerElem.done).to.equal(testSet.inner.expectedDone);
+          expect(innerElem.label).to.equal(testSet.inner.expectedLabel);
+          done();
+        });
+      });
+    });
+
+    it.only('should not reflect change to generated child without linked attribute', (done) => {
+      // inner component *wthout* linked attrs/props
+      const inner = element();
+      const innerTagName = inner.safe;
+      inner.skate({
+        props: {
+          label: prop.string(),
+          done: prop.boolean(),
+        },
+      });
+
+      // outer component which renders the inner component
+      const outer = element();
+      const outerTagName = outer.safe;
+      outer.skate({
+        render() {
+          vdom.element(innerTagName, { label: 'world', done: '' });
+        },
+      });
+
+      fixture(`
+        <${outerTagName}></${outerTagName}>
+      `);
+
+      afterMutations(() => {
+        const innerElem = document.querySelector(outerTagName).shadowRoot.querySelector(innerTagName);
+        expect(innerElem.getAttribute('label')).to.equal('world', 'attr');
+        expect(innerElem.getAttribute('done')).to.equal('', 'attr');
+        expect(innerElem.label).to.equal(null, 'prop');
+        expect(innerElem.done).to.equal(null, 'prop');
+        done();
+      }, 1);
     });
   });
 });
